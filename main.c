c8347f325ef69326ad5deab0cb135aa3905ef432
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-11-19 15:47:46 +0900

    03, 04

--- main.c
+++ main.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=0;
-	char str[4];
 	
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	printf("%s\n", str);
+	char str[30] = "happy C programming";
 	
-	//while(현재 문자 (str[i]}이 맨 끝이 아님) {
-	while (str[i] != '\0') {
-	//    printf로 현재 문자 하나 출력;
-	    printf("%c", str[i]);
-	//    i를 1 증가;
-	    i++;
-	//}
-    }
+	strcpy(dst, src);
+	
+	printf("copied string : %s\n", dst);
+	
+	printf("length of \"%s\" : %i\n", str, strlen(str));
+	
 	return 0;
 }
