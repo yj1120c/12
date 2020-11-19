b776f9ace2b9e78ba11baab4de53a72a4b79557b
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-11-19 16:02:34 +0900

    05

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	FILE *fp;
+	char str[100];
+	int i;
 	
-	char str[30] = "happy C programming";
+	fp = fopen("sample.txt", "w");
+	if (fp == NULL)
+	{
+		return -1;
+	}
 	
-	strcpy(dst, src);
-	
-	printf("copied string : %s\n", dst);
-	
-	printf("length of \"%s\" : %i\n", str, strlen(str));
-	
+	for (i=0;i<3;i++)
+	{
+		printf("Input a word :");
+		scanf("%s", str);
+		fprintf(fp, "%s\n", str);
+	}
+	fclose(fp);
+
 	return 0;
 }
