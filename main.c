92e6eed95faa46c4547fdf09960491c022d4a514
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-11-19 15:34:23 +0900

    02

--- main.c
+++ main.c
@@ -4,10 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char code1 = 'A';
-	int code2 = 65;
+	int i=0;
+	char str[4];
 	
-	printf("code1 = %c, (%d)\n", code1, code1);
-	printf("code2 = %c, (%d)\n", code2, code2);
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+	str[3] = '\0';
+	
+	printf("%s\n", str);
+	
+	//while(현재 문자 (str[i]}이 맨 끝이 아님) {
+	while (str[i] != '\0') {
+	//    printf로 현재 문자 하나 출력;
+	    printf("%c", str[i]);
+	//    i를 1 증가;
+	    i++;
+	//}
+    }
 	return 0;
 }
